6d4ec2323474c3b4b74c6f72233401637f4336be
fruGal9 <98467335+fruGal9@users.noreply.github.com>
2022-06-10 21:32:53 +0300

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,75 +1,44 @@
-const getUserChoice = userInput => {
-
-    userInput = userInput.toLowerCase();
-
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
-        return userInput;
-    } else {
-        console.log('Error!');
+const computerPlay = () => {
+    const options = ["rock", "paper", "scissors"];
+    const result = options[Math.floor(Math.random() * 3)];
+  
+    return result;
+  };
+  
+  const playround = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+  
+    if (playerSelection === computerSelection) return "tie";
+  
+    if (playerSelection === "rock")
+      return computerSelection === "scissors" ? "userWins" : "computerWins";
+  
+    if (playerSelection === "paper")
+      return computerSelection === "rock" ? "userWins" : "computerWins";
+  
+    if (playerSelection === "scissors")
+      return computerSelection === "paper" ? "userWins" : "computerWins";
+  };
+  
+  const computerSelection = computerPlay();
+  const playerSelection = "rock";
+  const result = playround(playerSelection, computerSelection);
+  
+  const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+      const computerSelection = computerPlay();
+      const playerSelection = prompt("Rock, paper, or scissors?");
+      const result = playround(playerSelection, computerSelection);
+  
+      result === "userWins" ? playerScore++ : computerScore++;
+      console.log(result);
     }
-
-};
-
-
-const getComputerChoice = () => {
-    const randomNumber = Math.floor(Math.random() * 3);
-
-
-    switch (randomNumber) {
-        case 0:
-            return 'rock';
-
-        case 1:
-            return 'paper';
-
-        case 2:
-            return 'scissors';
-
-    }
-
-}
-
-
-
-const determineWinner = (userChoice, computerChoice) => {
-
-    if (userChoice === computerChoice) {
-        return 'It\s a tie';
-    }
-
-    if (userChoice === 'rock') {
-        if (computerChoice === 'paper') {
-            return 'Computer Won';
-        }
-        else {
-            return 'Youw won';
-        }
-    }
-    if (userChoice === 'paper') {
-        if (computerChoice === 'scissors') {
-            return 'Computer won';
-
-        } else {
-            return 'You won';
-        }
-    }
-    if (userChoice === "scissors") {
-        if (computerChoice === 'rock') {
-            return 'Computer won';
-        } else {
-            return 'You won';
-        }
-    }
-
-};
-
-const playGame = () => {
-    let input = prompt('Choose your destiny', '')
-    const userChoice = getUserChoice(input);
-    const computerChoice = getComputerChoice();
-    console.log('You threw: ' + userChoice);
-    console.log('The computer threw:' + computerChoice);
-    console.log(determineWinner(userChoice, computerChoice));
-};
-
-playGame();
+  
+    console.log(`Player: ${playerScore} Computer: ${computerScore}`);
+    playerScore === computerScore && console.log("It's a tie!");
+    console.log(playerScore > computerScore ? "You win!" : "Computer wins");
+  };
+  
+  game();
