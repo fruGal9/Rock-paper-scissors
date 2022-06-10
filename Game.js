91c8ca0a431e8906469724b97775ccf6f2c2fde1
fruGal9 <98467335+fruGal9@users.noreply.github.com>
2022-06-10 21:48:37 +0300

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,44 +1,86 @@
-const computerPlay = () => {
-    const options = ["rock", "paper", "scissors"];
-    const result = options[Math.floor(Math.random() * 3)];
-  
-    return result;
-  };
-  
-  const playround = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
-  
-    if (playerSelection === computerSelection) return "tie";
-  
-    if (playerSelection === "rock")
-      return computerSelection === "scissors" ? "userWins" : "computerWins";
-  
-    if (playerSelection === "paper")
-      return computerSelection === "rock" ? "userWins" : "computerWins";
-  
-    if (playerSelection === "scissors")
-      return computerSelection === "paper" ? "userWins" : "computerWins";
-  };
-  
-  const computerSelection = computerPlay();
-  const playerSelection = "rock";
-  const result = playround(playerSelection, computerSelection);
-  
-  const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-      const computerSelection = computerPlay();
-      const playerSelection = prompt("Rock, paper, or scissors?");
-      const result = playround(playerSelection, computerSelection);
-  
-      result === "userWins" ? playerScore++ : computerScore++;
-      console.log(result);
-    }
-  
-    console.log(`Player: ${playerScore} Computer: ${computerScore}`);
-    playerScore === computerScore && console.log("It's a tie!");
-    console.log(playerScore > computerScore ? "You win!" : "Computer wins");
-  };
-  
-  game();
+console.log('Let\'s play!');
+game();
+
+function cpuPlay(){
+	let ranNumber = Math.random() * 300;
+	let cpuSelection = '';
+
+	if (ranNumber < 100){
+		cpuSelection = 'rock';
+	}
+	else if (ranNumber >= 100 && ranNumber < 200){
+		cpuSelection = 'paper';
+	}
+	else{
+		cpuSelection = 'scissors';
+	}
+	return (cpuSelection);
+}
+
+function playRound(playerSelection, computerSelection) {
+	let result = '';
+	if (playerSelection == 'rock' && computerSelection == 'paper'){
+		result = 'cpu';
+	}
+	else if (playerSelection == 'paper' && computerSelection == 'scissors'){
+		result = 'cpu';
+	}
+	else if (playerSelection == 'scissors' && computerSelection == 'rock'){
+		result = 'cpu';
+	}
+	else if (playerSelection == computerSelection){
+		result = 'tie';
+	}
+	else{
+		result = 'player';
+	}
+
+	return (result);
+}
+
+function game() {
+	let cpuScore = 0;
+	let playerScore = 0;
+	let winner = '';
+	for (let i = 0; i < 5; i++){
+		let computerSelection = cpuPlay();
+	
+		let playerSelection = prompt("Rock, Paper or Scissors?");
+	
+		if(playerSelection == null || playerSelection == ''){
+		
+			i--;
+			playerSelection = prompt("Rock, Paper or Scissors?");
+		}
+		else if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors'){
+			console.log("Wrong input!");
+			i--;
+			playerSelection = prompt("Rock, Paper or Scissors?");
+		}
+		else{
+			playerSelection = playerSelection.toLowerCase();
+			winner = playRound(playerSelection, computerSelection);
+			if (winner == 'player'){
+				console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+				playerScore++;
+			}
+			else if(winner == 'cpu'){
+				console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+				cpuScore++;
+			}
+			else{
+				console.log("It's a tie!");
+			}
+		}
+	}
+	console.log(`score(player - cpu): ${playerScore} - ${cpuScore}`);
+	if (playerScore > cpuScore){
+		console.log("You won!");
+	}
+	else if (cpuScore > playerScore){
+		console.log("You lose!");
+	}
+	else{
+		console.log("It's a tie!");
+	}
+}
